c8c21eb64cddf6ee6402ad9def1ed3b40c00ae4e
abbas679 <tahir76422@gmail.com>
2025-11-22 19:00:30 +0500

    Practice passing props and manipulations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import { ProductCard } from "./components/ProductCard";
+import { ProductList } from "./components/ProductList";
 import "./App.css";
 
 function App() {
+  const product = {
+    imageSrc: "images/iphone.png",
+    title: "iPhone 15 Pro",
+    altText: "iPhone 15 Pro",
+    specifications: [
+      "A17 Pro chip with 6-core GPU",
+      "3x or 5x Telephoto camera",
+      "Up to 29 hours video playback",
+    ],
+    price: 999,
+  };
+
   return (
     <div className="App">
-      <ProductCard />
+      <ProductList>
+        <ProductCard
+          widht="96px"
+          height="96px"
+          background="darkolivegreen"
+          product={product}
+        />
+        <ProductCard widht="64px" height="64px" product={product} />
+        <ProductCard
+          widht="128px"
+          height="128px"
+          background="peru"
+          product={product}
+        />
+      </ProductList>
     </div>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,35 +1,34 @@
-export function ProductCard() {
-  const product = {
-    imageSrc: "images/iphone.png",
-    title: "iPhone 15 Pro",
-    altText: "iPhone 15 Pro",
-    specifications: [
-      "A17 Pro chip with 6-core GPU",
-      "3x or 5x Telephoto camera",
-      "Up to 29 hours video playback",
-    ],
-    price: 999,
-  };
-
-  function getProductTitle(title) {
-    return title;
-  }
+export function ProductCard({
+  product,
+  background = "slategrey",
+  ...restProps
+}) {
   return (
-    <article>
-      <h2>{getProductTitle(product.title)}</h2>
-      <img
-        src={product.imageSrc}
-        alt={product.altText}
-        width="128px"
-        height="128px"
-      />
+    <article
+      style={{
+        background,
+        width: "100%",
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+        padding: "16px",
+        maxWidth: "200px",
+        textAlign: "center",
+      }}
+    >
+      <h2>{product.title}</h2>
+      <img src={product.imageSrc} alt={product.altText} {...restProps} />
       <p>Specification:</p>
-      <ul>
+      <ul
+        style={{
+          listStyle: "none",
+          padding: 0,
+        }}
+      >
         <li>{product.specifications[0]}</li>
         <li>{product.specifications[1]}</li>
         <li>{product.specifications[2]}</li>
       </ul>
-      <button>Buy (From{product.price})</button>
+      <button>Buy (From {product.price})</button>
     </article>
   );
 }
